b266cdbfcdfa7f79ecdcdfb0cc785ebd20b64220
Myles Byrne <myles@myles.id.au>
2013-12-05 16:40:24 -0600

    A remote login unit
    Intended to serve as an example for writing units that require
    manual steps

---
+++ remote-login/install.sh
@@ -0,0 +1,8 @@
+echo "\
+When system preferences is opened, click:
+* Sharing - last icon in third row, then;
+* The checkbox next to 'Remote Login' in the pane on the left"
+
+open "/Applications/System Preferences.app"
+
+read -p "Press enter when done"
