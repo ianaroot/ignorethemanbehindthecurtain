7af7581d3dea284c48824519490b0a1629a791cc
Myles Byrne <myles@myles.id.au>
2014-01-07 18:38:00 -0800

    Show current git user in prompt

---
+++ bash-config/dbc-ps1.sh
@@ -0,0 +1,45 @@
+function _ps1_join {
+  local s=''
+  for arg in $@
+    do s+=$arg
+  done
+  echo -n $s
+}
+
+function _ps1_git_username {
+  local git_user=`git config --global user.name`
+  test "$git_user" && echo "$git_user" && return
+  echo "No Git User"
+}
+
+function _ps1_git_repo_ref {
+  local ref=`git branch --no-color 2> /dev/null | sed 's/* //'`
+  test "$ref" && echo "$ref "
+}
+
+dark_gray='[0;38;5;238m'
+light_gray='[0;38;5;250m'
+reset_color='[m'
+
+space='\040'
+
+line1_segments=(
+  '\[' '\e' $dark_gray '\]'
+  # git user
+  '`_ps1_git_username`'
+  $space
+  '\[' '\e' $reset_color '\]'
+  # working dir
+  '\W'
+  '\[' '\e' $dark_gray '\]'
+  $space
+  # git ref
+  '`_ps1_git_repo_ref`'
+  '\[' '\e' $reset_color '\]'
+  '\[' '\e' $light_gray '\]'
+  '$'
+  '\[' '\e' $reset_color '\]'
+  $space
+)
+
+export PS1="`_ps1_join ${line1_segments[@]}`"
--- bash-config/install.sh
+++ bash-config/install.sh
@@ -1,6 +1,10 @@
 set -e
+
+dir=$(cd $(dirname $0); pwd)
+
 mkdir -p _resources
 cd _resources
 test -d bash-config || git clone https://github.com/quackingduck/bash-config.git
 cd bash-config
+cp $dir/dbc-ps1.sh ps1.sh # override the prompt with a dbc-specific one
 ./install
