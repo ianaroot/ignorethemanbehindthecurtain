1176c509e4cd39bb2cf38eb609d260cd272d6ace
Myles Byrne <myles@myles.id.au>
2014-01-14 19:14:43 -0800

    Force creation of subl symlink

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -1,2 +1,2 @@
 brew cask install sublime-text
-ln -s "$HOME/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+ln -fs "$HOME/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
