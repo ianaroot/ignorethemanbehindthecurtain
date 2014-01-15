c5af1c5b60796a4dedd5ea1201ac60526f5fa35a
Myles Byrne <myles@myles.id.au>
2014-01-14 19:17:49 -0800

    Only backup sublime conf if it exists

--- sublime-config/install.sh
+++ sublime-config/install.sh
@@ -3,5 +3,6 @@ mkdir -p _resources
 cd _resources
 test -d sublime-conf || git clone https://github.com/quackingduck/sublime-conf.git
 cd sublime-conf
-mv "$HOME/Library/Application Support/Sublime Text 2/Packages" /tmp/previous-sublime-packages
+test -d "$HOME/Library/Application Support/Sublime Text 2/Packages" &&
+  mv "$HOME/Library/Application Support/Sublime Text 2/Packages" /tmp/previous-sublime-packages
 ln -s `pwd` "$HOME/Library/Application Support/Sublime Text 2/Packages"
