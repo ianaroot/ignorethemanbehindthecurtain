f286cb73b14b66f72eb2c65611f6f2e47c3bf54f
Myles Byrne <myles@myles.id.au>
2014-01-27 14:00:26 -0800

    Clear confirm prompt
    We don't distribute this outside DBC so I don't think we need this warning

--- rspec-config/install.sh
+++ rspec-config/install.sh
@@ -1,8 +1,3 @@
 set -e
 
-! test -f "$HOME/.rspec" || {
-  read -p "This will replace ~/.rspec, press y to proceed: " confirm
-  test $confirm = 'y' -o $confirm = 'Y'
-}
-
 echo "--color --format documentation" > $HOME/.rspec
