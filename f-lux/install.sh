fdbd683c51ae2edc32eb0f99793048ab6b5c4304
Steven G. Harms <steven@devbootcamp.com>
2014-01-30 08:37:39 -0800

    Flux: Launch it post install to set data
    Flux needs to be launched to know your
    desaturation preferences and your geographic
    location.
    
    Let's go ahead and launch it post-install.

--- f-lux/install.sh
+++ f-lux/install.sh
@@ -1,2 +1,3 @@
 brew cask install f-lux
 osascript -e "tell application \"System Events\" to make new login item at end with properties {path:\"$HOME/Applications/Flux.app\", name:\"Flux\", hidden:false}"
+open -a $HOME/Applications/Flux.app
