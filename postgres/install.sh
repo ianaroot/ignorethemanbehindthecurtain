9589436ead822d4c1ef16d201b044a8e6f63ceee
Mike Busch <busch.michael@gmail.com>
2014-01-30 13:46:06 -0600

    use homebrew services command to manage running postgres
    Resolves #90

--- postgres/install.sh
+++ postgres/install.sh
@@ -1,4 +1,3 @@
 brew install postgres
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+brew services start postgres
 sleep 1
