d1ba2eed07cf5de05f8ff958a93602d1b22d1b05
Mike Busch <busch.michael@gmail.com>
2014-02-03 10:29:28 -0600

    add googlecast google chrome extension unit

---
+++ google-chrome-ext-googlecast/install.sh
@@ -0,0 +1,12 @@
+set -e
+
+extDir="$HOME/Library/Application Support/Google/Chrome/External Extensions"
+mkdir -p "$extDir"
+
+extId=`cat ./extension-id`
+extFile=$extDir'/'$extId'.json'
+
+# the update url was built by reverse engineering a plugin installed
+# through the Chrome Store.
+updateUrl='https://clients2.google.com/service/update2/crx?response=updatecheck&x=id%3D'$extId'%26uc'
+echo '{"external_update_url": "'$updateUrl'"}' > "$extFile"
