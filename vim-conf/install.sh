14992b86de5f8d1c1c89bf6388e998c9e7a5087f
Mike Busch <busch.michael@gmail.com>
2014-02-03 17:01:17 -0800

    no need to create a backup dir
    we don't save backups.

--- vim-conf/install.sh
+++ vim-conf/install.sh
@@ -8,10 +8,5 @@ set -e
 # Get the directory of the unit
 unit_dir=$(cd $(dirname $0); pwd)
 
-# Create a .vim directory and a backup directory since our basic .vimrc will
-# put backup files into it.  This helps prevent littering of swapfiles
-# everywhere.
-mkdir -p $HOME/.vim/backup
-
 # Copy the basic vimrc $HOME
 cp $unit_dir/basic-vimrc.vim ~/.vimrc
