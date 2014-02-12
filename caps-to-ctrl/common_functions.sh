b7a6db2303d17f3ae79dab109d1813f7646c8a70
salarkhan <salar.k3@gmail.com>
2014-02-11 22:22:38 -0800

    installed.sh now sources from the correct files instead of common_functions.sh         * definitely derped here. got too excited about refactoring install.sh         && forgot about installed.sh. whoops.

--- caps-to-ctrl/common_functions.sh
+++
@@ -1,9 +0,0 @@
-keyboard_ids=$(ioreg -n IOHIDKeyboard -r | grep -e VendorID\" -e ProductID | grep -o '\d.*')
-vendor_id_1=$(echo $keyboard_ids | awk '{print $1}')
-vendor_id_2=$(echo $keyboard_ids | awk '{print $3}')
-product_id_1=$(echo $keyboard_ids | awk '{print $2}')
-product_id_2=$(echo $keyboard_ids | awk '{print $4}')
-echo $vendor_id_1
-echo $product_id_1
-echo $vendor_id_2
-echo $product_id_2
