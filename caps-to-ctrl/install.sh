4d501b2f737c752c045841758d1c3963c0fc01c4
salarkhan <salar.k3@gmail.com>
2014-02-22 14:01:42 -0800

    Handle cases for 1 or 2 keyboards by checking wc of vendor_ids

--- caps-to-ctrl/install.sh
+++ caps-to-ctrl/install.sh
@@ -4,6 +4,11 @@ product_ids=$(./product_ids)
 keyboard_1=$(echo $vendor_ids $product_ids | awk '{print ($1, $3)}' | tr '\ ' -)
 keyboard_2=$(echo $vendor_ids $product_ids | awk '{print ($2, $4)}' | tr '\ ' -)
 
+if (($(echo $vendor_ids | wc -w)<2))
+    then
+    keyboard_1=$(echo $vendor_ids $product_ids | awk '{print ($1, $2)}' | tr '\ ' -)
+fi
+
 # http://apple.stackexchange.com/questions/13598/updating-modifier-key-mappings-through-defaults-command-tool
 function map_keys {
 defaults -currentHost write -g com.apple.keyboard.modifiermapping."$1"-0 -array-add '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'
--- caps-to-ctrl/installed.sh
+++ caps-to-ctrl/installed.sh
@@ -4,10 +4,14 @@ product_ids=$(./product_ids)
 keyboard_1=$(echo $vendor_ids $product_ids | awk '{print ($1, $3)}' | tr '\ ' -)
 keyboard_2=$(echo $vendor_ids $product_ids | awk '{print ($2, $4)}' | tr '\ ' -)
 
+if (($(echo $vendor_ids | wc -w)<2))
+    then
+    keyboard_1=$(echo $vendor_ids $product_ids | awk '{print ($1, $2)}' | tr '\ ' -)
+fi
+
 key_mappings_1=$(defaults -currentHost read -g "com.apple.keyboard.modifiermapping."$keyboard_1"-0")
 key_mappings_2=$(defaults -currentHost read -g "com.apple.keyboard.modifiermapping."$keyboard_2"-0")
 
 [[ $key_mappings_1 == *"HIDKeyboardModifierMappingDst = 2;
         HIDKeyboardModifierMappingSrc = 0;"* ]] && [[ $key_mappings_2 == *"HIDKeyboardModifierMappingDst = 2;
         HIDKeyboardModifierMappingSrc = 0;"* ]]
-
