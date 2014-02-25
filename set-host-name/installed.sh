9e1877073ae5ca1a9def9fdd0ec3c8ef5c8b8052
Topher Lubaway <asimplechris@gmail.com>
2014-02-25 15:33:47 -0800

    Yet another working dir fix.  Harder to diagnose, same problem though At this point i wonder if we can extract this out from each install.

--- set-host-name/installed.sh
+++ set-host-name/installed.sh
@@ -1,9 +1,9 @@
-dir=$(cd $(dirname $0); pwd)
+unit_dir=$(cd $(dirname $0); pwd)
 
 # Get this machine's mac address
 mac_address=`ifconfig en0 | grep '\bether\b' | sed 's/.ether //'`
 # Look up it's machine number
-host_num=`cat host-numbers-to-mac-addresses | grep '^\d' | grep $mac_address | cut -d ' ' -f 1`
+host_num=`cat $unit_dir/host-numbers-to-mac-addresses | grep '^\d' | grep $mac_address | cut -d ' ' -f 1`
 
 test $host_num || {
   echo "Unknown host number for this mac address ($mac_address). Is this a new machine?"
