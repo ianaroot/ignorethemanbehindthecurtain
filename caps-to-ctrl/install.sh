33240d97057785e996bae0c5e020c5685bb2f937
salarkhan <salar.k3@gmail.com>
2014-03-11 01:20:14 -0700

    install.sh now supports N keyboards     * consolidates vendor & product scripts into keyboard_ids script         * piped output into paste to get vendor/product ids on 1 line     * while loop feeds each line of keyboard_ids output into map_keys     * installed.sh checks for the last mapped dst/src keys, instead of     checking if the correct mapping was ever made

--- caps-to-ctrl/install.sh
+++ caps-to-ctrl/install.sh
@@ -1,18 +1,11 @@
-vendor_ids=$(./vendor_ids)
-product_ids=$(./product_ids)
-
-keyboard_1=$(echo $vendor_ids $product_ids | awk '{print ($1, $3)}' | tr '\ ' -)
-keyboard_2=$(echo $vendor_ids $product_ids | awk '{print ($2, $4)}' | tr '\ ' -)
-
-if (($(echo $vendor_ids | wc -w)<2))
-    then
-    keyboard_1=$(echo $vendor_ids $product_ids | awk '{print ($1, $2)}' | tr '\ ' -)
-fi
+keyboard_ids=$(./keyboard_ids)
 
 # http://apple.stackexchange.com/questions/13598/updating-modifier-key-mappings-through-defaults-command-tool
 function map_keys {
-defaults -currentHost write -g com.apple.keyboard.modifiermapping."$1"-0 -array-add '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'
+    defaults -currentHost write -g com.apple.keyboard.modifiermapping.$1-0 -array-add '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'
 }
 
-map_keys $keyboard_1
-map_keys $keyboard_2
+./keyboard_ids | while read vendor_id product_id
+do
+    map_keys $vendor_id-$product_id
+done
