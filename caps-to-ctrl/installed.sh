7f56d21acb34c3abd7a1e1c3f4c3a458c68c9cde
Myles Byrne <myles@myles.id.au>
2014-03-11 09:02:42 -0700

    Remove unused variable

--- caps-to-ctrl/installed.sh
+++ caps-to-ctrl/installed.sh
@@ -1,5 +1,3 @@
-keyboard_ids=$(./keyboard_ids)
-
 ./keyboard_ids | while read vendor_id product_id
 do
     [[ $(defaults -currentHost read -g com.apple.keyboard.modifiermapping.$vendor_id-$product_id-0) == *"HIDKeyboardModifierMappingDst = 2;
