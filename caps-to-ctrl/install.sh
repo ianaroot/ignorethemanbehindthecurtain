a66ef83217a04229b5c2e8330ec383bae33a77c4
Myles Byrne <myles@myles.id.au>
2014-03-11 09:03:16 -0700

    Remove unused variable

--- caps-to-ctrl/install.sh
+++ caps-to-ctrl/install.sh
@@ -1,5 +1,3 @@
-keyboard_ids=$(./keyboard_ids)
-
 # http://apple.stackexchange.com/questions/13598/updating-modifier-key-mappings-through-defaults-command-tool
 function map_keys {
     defaults -currentHost write -g com.apple.keyboard.modifiermapping.$1-0 -array-add '<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>'
