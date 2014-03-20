e1d83d893bab826c0bc8cee8664bfcec5b43cf6c
matt <mbaker.pdx@gmail.com>
2014-03-20 08:44:41 -0500

    Bring git-config into kitout instead of pulling from Nate's repo

--- git-config/install.sh
+++ git-config/install.sh
@@ -1,6 +1,11 @@
-set -e
-mkdir -p _resources
-cd _resources
-test -d git-config || git clone https://github.com/ndelage/git-config.git
-cd git-config
-./install
+#!/bin/sh
+
+read -p "This will delete ~/.gitconfig press y to proceed: " confirm
+
+dir=$(cd $(dirname $0); pwd)
+
+test $confirm = 'y' -o $confirm = 'Y' && {
+
+  rm -rf $HOME/.gitconfig
+  ln -s "$dir/gitconfig" $HOME/.gitconfig
+}
