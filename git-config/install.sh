45040efef33b759b9e8a7122a5ac1d5cf91bf36c
Alex Flores & Myles Byrne <pair+audibleblink.quackingduck@devbootcamp.com>
2014-03-28 15:06:05 -0400

    Installers don't need to be interactive in kitout

--- git-config/install.sh
+++ git-config/install.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-read -p "This will delete ~/.gitconfig press y to proceed: " confirm
-
 dir=$(cd $(dirname $0); pwd)
 
-test $confirm = 'y' -o $confirm = 'Y' && {
-
-  rm -rf $HOME/.gitconfig
-  ln -s "$dir/gitconfig" $HOME/.gitconfig
-}
+rm -rf $HOME/.gitconfig
+ln -s "$dir/gitconfig" $HOME/.gitconfig
