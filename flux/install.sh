9aae1e159ce420da46d4ebd6bc788218542b5078
Topher Lubaway <asimplechris@gmail.com>
2014-04-29 12:50:26 -0400

    fixed typo in install of flux. renamed folder to match correct spelling

--- flux/install.sh
+++ flux/install.sh
@@ -1,3 +1,3 @@
-brew cask install f-lux
+brew cask install flux
 osascript -e "tell application \"System Events\" to make new login item at end with properties {path:\"$HOME/Applications/Flux.app\", name:\"Flux\", hidden:false}"
 open -a $HOME/Applications/Flux.app
