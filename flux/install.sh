e48fc5dbf8a30210c2062caa8200cb5e1a57d239
Myles Byrne <myles@myles.id.au>
2014-05-14 01:07:43 -0500

    Force flux install to support re-install

--- flux/install.sh
+++ flux/install.sh
@@ -1,3 +1,4 @@
-brew cask install flux
+set -e
+brew cask install --force flux
 osascript -e "tell application \"System Events\" to make new login item at end with properties {path:\"$HOME/Applications/Flux.app\", name:\"Flux\", hidden:false}"
 open -a $HOME/Applications/Flux.app
