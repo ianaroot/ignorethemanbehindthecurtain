444800302ce4a0d0241105e1deb616e70a550d13
Christopher Lubaway <asimplechris@gmail.com>
2014-10-22 17:26:29 -0700

    Removes support of brew services from postgres install brew services has been depricated. tested locally.

--- postgres/install.sh
+++ postgres/install.sh
@@ -1,4 +1,5 @@
 brew install postgres
 mkdir -p $HOME/Library/LaunchAgents
-brew services start postgres
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 sleep 1
