c6b3446c85a96c8ff513bc872071ec9cf3c74a34
Christopher Lubaway <asimplechris@gmail.com>
2015-02-13 17:47:57 -0800

    WIP ruby 2 =/

--- set-ruby-2/install.sh
+++ set-ruby-2/install.sh
@@ -1,3 +1,4 @@
 # If rbenv if not currently in the path, add it
 echo $PATH | grep --quiet rbenv || PATH="$HOME/.rbenv/bin:$PATH"
+test -d "$HOME/.rbenv" && eval "$(rbenv init -)"
 rbenv global 2.0.0-p353
