bc9d24f9514b67a73ac5b29525e3f5e5936015de
Christopher Lubaway <asimplechris@gmail.com>
2015-02-17 11:28:18 -0800

    Moves git dep to proper location... avoiding a recursive dep Also changes a readlink line for simplicity

--- bash-config-ext-prompt/installed.sh
+++ bash-config-ext-prompt/installed.sh
@@ -1,4 +1,4 @@
 set -e
 dir=$(cd $(dirname $0); pwd)
 # compare the two files and test their diff
-cmp --silent `readlink $HOME/.bash_profile.d/prompt.sh` "$dir/prompt.sh"
+cmp --silent "$HOME/.bash_profile.d/prompt.sh" "$dir/prompt.sh"
